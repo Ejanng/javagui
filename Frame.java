082ca3fc20ed0e1f9d8cb5c43ac4614c778d61ed
yahalloe <lexuzherrera@gmail.com>
2024-05-18 13:48:23 +0800

    add setralative to null in frame i think

--- Frame.java
+++ Frame.java
@@ -11,6 +11,7 @@ public class Frame extends JFrame {
         
         pack();
         setVisible(true);
+        setLocationRelativeTo(null);
         // setLocationRelativeTo(null);
     }
 }
--- Panel.java
+++ Panel.java
@@ -66,7 +66,7 @@ public class Panel extends JPanel {
 
         add(taskPanel, BorderLayout.CENTER);
 
-
+        
 
 
     }
